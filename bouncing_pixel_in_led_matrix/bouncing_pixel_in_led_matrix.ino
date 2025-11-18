56757fa99e4a79ea8a073d17020264d37dc3d7b0
Gaurav Prasad <gauravprasad003@gmail.com>
2025-11-18 19:32:48 +0530

    Added last edge position of bouncing pixel in LED Matrix

--- bouncing_pixel_in_led_matrix/bouncing_pixel_in_led_matrix.ino
+++ bouncing_pixel_in_led_matrix/bouncing_pixel_in_led_matrix.ino
@@ -7,6 +7,10 @@ int yPos=2;
 int deltaX=1;
 int deltaY=1;
 
+// Last Position
+int lastXPos = -1;
+int lastYPos = -1;
+
 byte frame[8][12];
 
 int delayTime=100;
@@ -25,6 +29,11 @@ void loop() {
       if(i==xPos && j==yPos) {
         frame[j][i]=1;
       }
+
+      // Last Position
+      if(i==lastXPos && j==lastYPos) {
+        frame[j][i]=1;
+      }
     }
   }
 
@@ -32,10 +41,18 @@ void loop() {
 
   if(xPos==0 || xPos==11) {
     deltaX = -deltaX;
+
+    // Last Position
+    lastXPos = xPos;
+    lastYPos = yPos;
   }
 
   if(yPos==0 || yPos==7) {
     deltaY = -deltaY;
+
+    // Last Position
+    lastXPos = xPos;
+    lastYPos = yPos;
   }
 
   xPos = xPos + deltaX;
