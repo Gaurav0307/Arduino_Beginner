290e127ac61e9995c245248beeef46fc968e3ea3
Gaurav Prasad <gauravprasad003@gmail.com>
2026-04-18 21:17:05 +0530

    NRF24L01 Transceiver Arduino & ESP32

---
+++ NRF24L01_Transceiver_Arduino/NRF24L01_Transceiver_Arduino.ino
@@ -0,0 +1,67 @@
+/*
+🔌Wiring (Important)
+ NRF24L01 → Arduino (same for both boards)
+
+| NRF24L01 Pin | Arduino Pin      |
+| ------------ | ---------------- |
+| VCC          | 3.3V (not 5v)    |
+| GND          | GND              |
+| CE           | D9               |
+| CSN          | D10              |
+| SCK          | D13              |
+| MOSI         | D11              |
+| MISO         | D12              |
+
+*/
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+RF24 radio(9, 10); // CE, CSN
+
+const byte address[6] = "GP003";
+
+const String type = "Transmitter";
+// const String type = "Receiver";
+
+#define LED_PIN 2
+
+void setup() {
+  Serial.begin(9600);
+  radio.begin();
+  if(type.equals("Transmitter")) {
+    radio.openWritingPipe(address);
+    radio.setChannel(108);
+    radio.setDataRate(RF24_250KBPS);
+    radio.setPALevel(RF24_PA_MIN);
+    radio.stopListening();
+  } else if(type == "Receiver") {
+    radio.openReadingPipe(0, address);
+    radio.setChannel(108);
+    radio.setDataRate(RF24_250KBPS);
+    radio.setPALevel(RF24_PA_MIN);
+    radio.startListening();
+  }
+
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  if(type.equals("Transmitter")) {
+    const char text[] = "Hello World";
+    radio.write(&text, sizeof(text));
+    Serial.println("Sent: " + String(text));
+    delay(1000);
+  } else if(type == "Receiver") {
+    if (radio.available()) {
+      char text[32] = "";
+      radio.read(&text, sizeof(text));
+      Serial.println("Received: " + String(text));
+
+      digitalWrite(LED_PIN, HIGH);
+      delay(500);
+      digitalWrite(LED_PIN, LOW);
+    }
+  }
+}
---
+++ NRF24L01_Transceiver_ESP32/NRF24L01_Transceiver_ESP32.ino
@@ -0,0 +1,70 @@
+/*
+🔌Wiring (Important)
+ NRF24L01 → ESP32 (same for both boards)
+
+| NRF24L01 | ESP32-C3 Mini |
+| -------- | ------------- |
+| VCC      | 3.3V          |
+| GND      | GND           |
+| CE       | GPIO 7        |
+| CSN      | GPIO 10       |
+| SCK      | GPIO 4        |
+| MOSI     | GPIO 6        |
+| MISO     | GPIO 5        |
+
+*/
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+#define CE_PIN 7
+#define CSN_PIN 10
+
+RF24 radio(CE_PIN, CSN_PIN);
+
+const byte address[6] = "GP003";
+
+const String type = "Receiver";
+// const String type = "Transmitter";
+
+#define LED_PIN 8
+
+void setup() {
+  Serial.begin(115200);
+  radio.begin();
+  if(type == "Transmitter") {
+    radio.openWritingPipe(address);
+    radio.setChannel(108);
+    radio.setDataRate(RF24_250KBPS);
+    radio.setPALevel(RF24_PA_LOW);
+    radio.stopListening();
+  } else if(type.equals("Receiver")) {
+    radio.openReadingPipe(0, address);
+    radio.setChannel(108);
+    radio.setDataRate(RF24_250KBPS);
+    radio.setPALevel(RF24_PA_LOW);
+    radio.startListening();
+  }
+
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  if(type == "Transmitter") {
+    const char text[] = "Hello World";
+    radio.write(&text, sizeof(text));
+    Serial.println("Sent: " + String(text));
+    delay(1000);
+  } else if(type.equals("Receiver")) {
+    if (radio.available()) {
+      char text[32] = "";
+      radio.read(&text, sizeof(text));
+      Serial.println("Received: " + String(text));
+
+      digitalWrite(LED_PIN, HIGH);
+      delay(500);
+      digitalWrite(LED_PIN, LOW);
+    }
+  }
+}
